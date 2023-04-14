e5a7b588d2b390ce783dafc957efb9bbb4f62859
dom111 <dom111@users.noreply.github.com>
2023-04-14 19:24:54 +0100

    Ensure `Moved` `Rule` is triggered after a `Unit` is `Disband`ed.

--- Disband.js
+++ Disband.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Disband = void 0;
 const Action_1 = require("@civ-clone/core-unit/Action");
+const Moved_1 = require("@civ-clone/core-unit/Rules/Moved");
 class Disband extends Action_1.default {
     perform() {
         this.unit().destroy();
+        this.ruleRegistry().process(Moved_1.default, this.unit(), this);
     }
 }
 exports.Disband = Disband;
--- Disband.ts
+++ Disband.ts
@@ -1,8 +1,11 @@
 import Action from '@civ-clone/core-unit/Action';
+import Moved from '@civ-clone/core-unit/Rules/Moved';
 
 export class Disband extends Action {
   perform() {
     this.unit().destroy();
+
+    this.ruleRegistry().process(Moved, this.unit(), this);
   }
 }
 
